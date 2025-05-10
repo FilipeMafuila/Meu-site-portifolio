1d57070762ad3b51bda138e44f0df2b5a8a76dd4
FilipeMafuila <filipemafuila8@gmail.com>
2025-05-10 12:06:49 +0100

    atualizei
    Subi os arquivos!

---
+++ aula.js
@@ -0,0 +1,28 @@
+ScrollReveal().reveal('#cards', {
+    origin:'bottom',
+    distance:'20px',
+    duration:3000,
+    delay :200,
+    reset: false
+})
+ScrollReveal().reveal('#tec', {
+    origin:'bottom',
+    distance:'50px',
+    duration:2000,
+    delay :200,
+    reset: false
+})
+ScrollReveal().reveal('#sb', {
+    origin:'bottom',
+    distance:'50px',
+    duration:2000,
+    delay :200,
+    reset: false
+})
+ScrollReveal().reveal('#form', {
+    origin:'bottom',
+    distance:'50px',
+    duration:2000,
+    delay :200,
+    reset: false
+})
